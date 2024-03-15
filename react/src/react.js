3387160b981932968ca3dcb964975b0cbc2310d1
도디의 라이프스타일 <kangdoroth2500@gmail.com>
2024-03-15 12:23:43 +0900

    4강 2회독

--- react/src/react.js
+++ react/src/react.js
@@ -14,6 +14,13 @@ export function createDOM(node){
     return element;
   }
 
+export function createElement(tag, props, ...children){
+  // children을 가변인자로 받으면 배열로만들어진다?
+  // 미친 이렇게 펼치니까 배열이 만들어졌어.. 
+  console.log('children', children)
+  return { tag, props, children }
+}
+
 export function render(vdom, container){
   container.appendChild(createDOM(vdom)); 
 }
